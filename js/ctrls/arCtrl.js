389c465ae99a1abbacd325cb88d16207a7a5cecc
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-03-08 13:36:47 +0800

    down cuality of screenshot image

--- js/ctrls/arCtrl.js
+++ js/ctrls/arCtrl.js
@@ -56,14 +56,12 @@ app.controller('ArCtrl', ["$scope", "$window",
             ctx2.drawImage(c, 0, 0, ($(window).width()), ($(window).height()));
             ctx2.drawImage(canvas, 0, 0, ($(window).width()), ($(window).height()));
 
-            // Canvas2Image.saveAsJPEG(canvasFinal, ($(window).width()), ($(window).height()));
-
             body.appendChild(canvasFinal);
 
             canvasToImage('myCanvas', {
                 name: 'dirstuffAR',
                 type: 'jpg',
-                quality: 1
+                quality: 0.7
             });
         };
     }
